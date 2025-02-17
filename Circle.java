ebfb72c1cc19e7436ff6e3f17f8d6ff286f9b83e
QuocNv201 <quocnv.24ce@vku.udn.vn>
2025-02-17 23:27:21 +0700

    Bai1.1

--- Circle.java
+++ Circle.java
@@ -1,13 +1,8 @@
-<<<<<<< HEAD
-=======
-package Circle;
->>>>>>> d275052 (Bai 1.1)
 
+package Circle;
 public class Circle {
 	private double radius;
 	private String color;
-	
-<<<<<<< HEAD
 public Circle() {
 	radius = 1.0;
 	color = "red";
@@ -29,12 +24,10 @@ public double getArea() {
 public String getColor() {
 	   return color;
 }
-/** Setter for instance variable radius */
 public void setRadius(double newRadius) {
-   this.radius = newRadius;   // "this.radius" tham chiếu biến instance
+   this.radius = newRadius;  
 }
 
-/** Setter for instance variable color */
 public void setColor(String newColor) {
    this.color = newColor;
 }
@@ -43,36 +36,3 @@ public String toString() {
 	}
 }
 
-=======
-	public Circle() {
-		radius = 1.0;
-		color = "red";
-	}
-	public Circle(double radius) {
-		this.radius = radius;
-		color = "red";
-	}
-	public Circle(double radius, String color) {
-		this.radius = radius;
-		color = "red";
-	}
-	public double getRadius() {
-		return radius;
-	}
-	public String getColor() {
-		return color;
-	}
-	public void setRadius(double newRadius) {
-		radius = newRadius;
-	}
-	public void setColor(String newColor) {
-		color = newColor;
-	}
-	public String toString() {
-		return " Circle[radius = " + radius +" color " + color +"]";
-	}
-	public double getArea() {
-		return radius*radius*Math.PI;
-	}
-}
->>>>>>> d275052 (Bai 1.1)
--- TestCircle.java
+++ TestCircle.java
@@ -1,13 +1,9 @@
-<<<<<<< HEAD
-import java.security.PublicKey;
-=======
+
 package Circle;
->>>>>>> d275052 (Bai 1.1)
 
 public class TestCircle {
 	public static void main(String[] args) {
 		Circle c1 = new Circle();
-<<<<<<< HEAD
 	    System.out.println(c1);
 		System.out.println(" The circle has radius of "
 				+ c1.getRadius() + " and area of "+ c1.getArea());
@@ -22,21 +18,6 @@ public class TestCircle {
         System.out.println(" Color is: " + c4.getColor());
 	    System.out.println(c4);
 	    System.out.println(" After modification: " + c4);
-=======
-		System.out.println(" The circle has radius of "+ c1.getRadius() + " and area of " + c1.getArea() );
-		Circle c2 = new Circle(2.0);
-		System.out.println(" The circle has radius of "+ c2.getRadius() + " and area of " + c2.getArea() );
-		Circle c4 = new Circle();
-		c4.setRadius(5.5);
-		System.out.println(" radius is: "+ c4.getRadius());
-		c4.setColor("green");
-		System.out.println(" color is: "+ c4.getColor());
-		Circle c5 = new Circle(5.5);
-		System.out.println(c5.toString());
-		Circle c6 = new Circle(6.6);
-		System.out.println(c6);
-		System.out.println(" Operator '+' invokes toString() too:"+ c6 );
-		
->>>>>>> d275052 (Bai 1.1)
+
 	}
 }
